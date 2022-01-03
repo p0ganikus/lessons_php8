f52c4771ef63429984fdc5ffa0a1cb6b448d2deb
p0ganikus <poganikus@dmail.com>
2022-01-03 14:41:19 +0300

    delete lorem

--- index.php
+++ index.php
@@ -2,5 +2,3 @@
 // git add . - Добавить все файлы
 // git commit -m "first commit" - Добавить commit
 echo 'Hello world git push -u origin main';
-?>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae delectus doloremque earum hic iste minima optio possimus ut? Accusamus consectetur culpa dolorem incidunt maxime nisi nulla quis quisquam rem unde!
